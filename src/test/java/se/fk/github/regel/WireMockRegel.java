45fc065c4e7c27e746fb07c685e87901949a10f5
Lars Persson <254442132+larpersso@users.noreply.github.com>
2026-04-22 10:04:25 +0200

    fix: Adjust template in preparation for release

---
+++ src/test/java/se/fk/github/regel/WireMockRegel.java
@@ -0,0 +1,18 @@
+package se.fk.github.regel;
+
+import com.github.tomakehurst.wiremock.WireMockServer;
+import se.fk.rimfrost.framework.regel.maskinell.WireMockRegelMaskinell;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class WireMockRegel extends WireMockRegelMaskinell
+{
+   @Override
+   protected Map<String, String> wiremockMapping(WireMockServer server)
+   {
+      Map<String, String> map = new HashMap<>(super.wiremockMapping(server));
+      // TODO: Add any rule-specific base-urls that needs mocking to the map. Ex: map.put("abc.api.base-url", server.baseUrl())
+      return map;
+   }
+}
